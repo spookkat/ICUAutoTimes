166e2f053ffb0d98f728bf3dec2a2a003c40c1ff
sujaysp99 <sujayspofficial@gmail.com>
2025-08-01 15:45:48 -0500

    Modifying vital_to_csv conversion

--- convert_vital_to_csv.py
+++ convert_vital_to_csv.py
@@ -2,7 +2,7 @@ import argparse
 import vitaldb as vdb
 import os
 
-def vital_to_csv_file(file_path, out_file_path, track_names=[]):
+def vital_to_csv_file(file_path, out_file_path, track_names=[], write_meta=False, delete_original=False):
     load_tracks = track_names
     if len(track_names) == 0:
         load_tracks = None
@@ -10,22 +10,51 @@ def vital_to_csv_file(file_path, out_file_path, track_names=[]):
     vdb_data = vdb_data.fillna(method='ffill', axis=0).fillna(method='bfill', axis=0)
     vdb_data = vdb_data.rename(columns={'Time': 'date'})
     vdb_data.to_csv(out_file_path, index=False)
+    vdb_len = len(vdb_data)
+    del vdb_data
 
-def vital_to_csv_folder(folder_path, out_folder_path, track_names=[]):
+    if delete_original:
+        try:
+            os.remove(file_path)
+            #print(f"File '{file_path}' deleted successfully.")
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    if write_meta:
+        return vdb_len
+
+def vital_to_csv_folder(folder_path, out_folder_path, track_names=[], write_meta=False, delete_original=False):
     file_list = os.listdir(folder_path)
     os.makedirs(out_folder_path, exist_ok=True)
+    total_len = 0
+    if write_meta:
+        data_meta = open(os.path.join(out_folder_path, 'data_meta.txt'), 'w')
     for filename in file_list:
         out_file_name = f"{filename.split('.')[0]}.csv"
-        vital_to_csv_file(os.path.join(folder_path, filename), os.path.join(out_folder_path, out_file_name), track_names)
+        vdb_len = vital_to_csv_file(os.path.join(folder_path, filename), os.path.join(out_folder_path, out_file_name), track_names, write_meta, delete_original)
+        if write_meta:
+            if vdb_len:
+                data_meta.writelines([f"{filename.split('.')[0]} {vdb_len}\n"])
+                total_len += vdb_len
+    
+    if write_meta:
+        data_meta.writelines([f"TOTAL {total_len}"])
+        data_meta.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Dataset Conversion")
     parser.add_argument('--data_folder_path', type=str, default='data/', help='dataset folder path')
     parser.add_argument('--data_output_path', type=str, default='out_data/', help='dataset folder path')
+    parser.add_argument('--write_meta', action='store_true', default=False, help='Should write meta data')
+    parser.add_argument('--delete_original', action='store_true', default=False, help='Should delete original after writing files')
     args = parser.parse_args()
 
     data_folder_path = args.data_folder_path
     data_output_path = args.data_output_path
+    write_meta = args.write_meta
+    delete_original = args.delete_original
 
     vital_track_names = [
         'SNUADC/ART',
@@ -37,4 +66,4 @@ if __name__ == "__main__":
         'BIS/EEG2_WAV'
     ]
 
-    vital_to_csv_folder(data_folder_path, data_output_path, vital_track_names)
+    vital_to_csv_folder(data_folder_path, data_output_path, vital_track_names, write_meta, delete_original)
